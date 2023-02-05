3a9009928af00df3d761948db6bbecf3b8073c75
1024th <lintianchuan@foxmail.com>
2023-02-05 12:34:32 +0800

    add MergeBlock in SimplifyCFG

--- src/middleend/SimplifyCFG.java
+++ src/middleend/SimplifyCFG.java
@@ -1,11 +1,14 @@
 package middleend;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 import ir.BasicBlock;
 
 /**
  * <ul>
+ * <li>Merges a basic block into its predecessor if there is only one and the
+ * predecessor only has one successor.
  * <li>Removes basic blocks with no predecessors.
  * <li>Eliminates PHI nodes for basic blocks with a single predecessor.
  */
@@ -16,6 +19,10 @@ public class SimplifyCFG {
 
   void runOnFunc(ir.Function func) {
     boolean changed = true;
+    while (changed) {
+      new CFGBuilder().runOnFunc(func);
+      changed = mergeBlock(func);
+    }
     while (changed) {
       new CFGBuilder().runOnFunc(func);
       changed = removeDeadBlock(func);
@@ -67,5 +74,49 @@ public class SimplifyCFG {
         phi.replaceAllUsesWith(phi.getOperand(0));
       }
     }
+  }
+
+  boolean mergeBlock(ir.Function func) {
+    blockMoveMap.clear();
+    for (var pred : func.blocks) {
+      pred = getAlias(pred);
+      if (pred.nexts.size() != 1)
+        continue;
+      var succ = pred.nexts.get(0);
+      if (succ.prevs.size() != 1)
+        continue;
+      if (func.exitBlock == succ)
+        func.exitBlock = pred;
+      pred.insts.remove(pred.insts.size() - 1);
+      assert succ.phiInsts.isEmpty();
+      for (var sucInst : succ.insts) {
+        sucInst.parent = pred;
+        pred.insts.add(sucInst);
+      }
+      pred.nexts.clear();
+      pred.nexts.addAll(succ.nexts);
+      for (var suc : succ.nexts) {
+        suc.redirectPred(succ, pred);
+      }
+      blockMoveMap.put(succ, pred);
+    }
+    var iter = func.blocks.iterator();
+    while (iter.hasNext()) {
+      var block = iter.next();
+      if (blockMoveMap.containsKey(block))
+        iter.remove();
+    }
+    return blockMoveMap.size() != 0;
+  }
+
+  HashMap<BasicBlock, BasicBlock> blockMoveMap = new HashMap<>();
+
+  BasicBlock getAlias(BasicBlock block) {
+    var a = blockMoveMap.get(block);
+    if (a == null)
+      return block;
+    a = getAlias(a);
+    blockMoveMap.put(block, a);
+    return a;
   }
 }
